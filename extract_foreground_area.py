9c93a42ae72ff6dd1f9a1cf37a7baed0babe41f5
hrkd1316 <hrkd1316@gmai.com>
2019-05-14 19:13:51 +0900

    [add] function to extract foreground of image

--- extract_foreground_area.py
+++ extract_foreground_area.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
+
 # 参照:http://urx2.nu/ZHbM (OpenCV Tutorials)
 def extract_foreground_area(input_img, rect_cover_foreground):
-    mask_background = np.zeros(input_img.shape[:2], np.uint8)
-
+    mask_img = np.zeros(input_img.shape[:2], np.uint8)
+    
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
     
     # cv2.grabCut(img, mask, rect, bgdModel, fgdModel, iterCount, mode)
-    cv2.grabCut(input_img, mask_background, rect_cover_foreground, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+    cv2.grabCut(input_img, mask_img, rect_cover_foreground, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
-    mask_background2 = np.where((mask_background == 2) | (mask_background == 0), 0, 1).astype('uint8')
-    output_img = input_img * mask_background2[:, :, np.newaxis]
+    mask_img2 = np.where((mask_img == 2) | (mask_img == 0), 0, 1).astype('uint8')
+    output_img = input_img * mask_img2[:, :, np.newaxis]
     return output_img
